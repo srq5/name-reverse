fdc1b293c4fc4d2d9d0a079834747fe20442c60c
Steven <stevenesammon@hotmail.com>
2018-02-04 19:57:01 -0500

    files

---
+++ NameReverse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace NameReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter your name to see its reverse.");
+
+
+                string name = Console.ReadLine().ToUpper();
+                char[] nameArray = name.ToCharArray();
+
+
+                Array.Reverse(nameArray);
+
+                foreach (char nameChar in nameArray)
+                {
+                    Console.Write(nameChar);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("       Another name? [Y or N]");
+                Console.ResetColor();
+
+                string playAgainAnswer = Console.ReadLine().ToUpper();
+
+
+                if (playAgainAnswer == "Y")
+                {
+                    continue;
+
+                }
+                else if (playAgainAnswer == "N")
+                {
+
+                    return;
+
+                }
+                else
+                {
+                    return;
+                }
+                Console.ReadLine();
+
+
+
+            }
+
+        }
+    }
+
+}
